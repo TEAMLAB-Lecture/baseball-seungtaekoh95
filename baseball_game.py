776de385028011f1de660b33431115ad4eb5433f
seungtaekoh95 <seungtaekoh95@gmail.com>
2021-01-21 23:43:06 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -113,7 +113,7 @@ def is_validated_number(user_input_number):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
     result = False
-    if is_digit(user_input_number) and is_between_100_and_999(user_input_number) and is_duplicate_number(user_input_number) == False:
+    if is_digit(user_input_number) and is_between_100_and_999(user_input_number) and is_duplicated_number(user_input_number) == False:
         result = True
     # ==================================
     return result
