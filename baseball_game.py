b09f1ef9a2a3395bab96cace4cb04a784c91ea0c
seungtaekoh95 <seungtaekoh95@gmail.com>
2021-01-21 15:56:15 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -31,8 +31,7 @@ def is_digit(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    result = None
-
+    result = user_input_number.isdigit()
     # ==================================
     return result
 
@@ -58,8 +57,7 @@ def is_between_100_and_999(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    result = None
-
+    result = True if (user_input_number>=100) and (user_input_number<1000) else False 
     # ==================================
     return result
 
@@ -86,8 +84,7 @@ def is_duplicated_number(three_digit):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    result = True if len(set(three_digit))!= 3 else False
     # ==================================
     return result
 
@@ -114,8 +111,9 @@ def is_validated_number(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    result = False
+    if is_digit(user_input_number) and is_between_100_and_999(user_input_number) and is_duplicate_number(user_input_number) == False:
+        result = True
     # ==================================
     return result
 
@@ -141,8 +139,10 @@ def get_not_duplicated_three_digit_number():
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
     # get_random_number() 함수를 사용하여 random number 생성
-
-    result = None
+    random_number = get_random_number()
+    while len(set(str(random_number)))!=3:
+        random_number = get_random_number()
+    result = random_number
     # ==================================
     return result
 
@@ -174,8 +174,13 @@ def get_strikes_or_ball(user_input_number, random_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    result = [0, 0]
+    for index, input_num in enumerate(user_input_number):
+        for index2, random_num in enumerate(random_number):
+            if index == index2 and input_num == random_num:
+                result[0] += 1
+            elif index != index2 and input_num == random_num:
+                result[1] += 1
     # ==================================
     return result
 
@@ -207,7 +212,8 @@ def is_yes(one_more_input):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
 
-    result = None
+    result = False
+    
     # ==================================
     return result
 
